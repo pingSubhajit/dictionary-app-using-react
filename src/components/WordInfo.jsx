6791b140d630dd070b095618e5551a25dfa3ad4d
Subhajit Kundu <37709181+pingSubhajit@users.noreply.github.com>
2024-01-24 21:21:22 +0530

    Update WordInfo.jsx

--- src/components/WordInfo.jsx
+++ src/components/WordInfo.jsx
@@ -1,6 +1,6 @@
 const WordInfo = ({definition}) => {
     return (
-        <div className="word-info">
+        <div data-testid='word-info' className="word-info">
             <h3>Example Definition</h3>
         </div>
     )
