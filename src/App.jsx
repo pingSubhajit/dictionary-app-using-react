7d9cf757e51a530f49376d1344cfc327cdcaf475
Subhajit Kundu <subha60kundu@gmail.com>
2024-01-24 13:19:08 +0530

    init: lab solution for getting started module

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Header from "./components/Header.jsx";
+import "./App.css";
+
 function App() {
 	return (
-		<>
+		<main>
+			<Header />
 			<h1>Welcome to Dictionary Application using React</h1>
-		</>
+		</main>
 	);
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,12 @@
-const Header = () => {
+import dictionaryLogo from '/dictionary.svg';
+import './Header.css';
 
+const Header = () => {
+    return (
+        <header data-testid='header' className='header'>
+            <img src={dictionaryLogo} alt='Dictionary Logo' className='header__logo' />
+        </header>
+    )
 };
 
 export default Header;
