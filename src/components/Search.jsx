65ab366bc99a318ab5108994d418b57954d6a750
Subhajit Kundu <subha60kundu@gmail.com>
2024-01-24 16:07:17 +0530

    init: Solution for building the search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,25 @@
+import searchIcon from '/searchIcon.svg'
+import './Search.css'
+
+const Search = ({searchValue, setSearchValue}) => {
+    const handleSearchInputChanges = (e) => {
+        setSearchValue(e.target.value)
+    }
+
+    return (
+        <div data-testid="search" className='search'>
+            <input
+                value={searchValue}
+                onChange={handleSearchInputChanges}
+                type="text"
+                placeholder='Search for a word'
+                className='search__input'
+            />
+            <button type="submit" className='search__button'>
+                <img src={searchIcon} alt='Search icon' className='search__button__icon' />
+            </button>
+        </div>
+    )
+}
+
+export default Search
