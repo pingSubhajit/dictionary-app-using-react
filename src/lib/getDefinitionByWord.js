9cfc4142c7b7fdfe9a7514bba63bb63155dd79ea
Subhajit Kundu <subha60kundu@gmail.com>
2024-01-24 23:51:31 +0530

    indentation fix

--- src/lib/getDefinitionByWord.js
+++ src/lib/getDefinitionByWord.js
@@ -1,6 +1,10 @@
 export const getDefinitionByWord = async (word) => {
     if (!word) return null
     const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+
     if (response.ok) return await response.json()
-    else return { error: true, detail: await response.json() }
+    else return {
+        error: true,
+        detail: await response.json()
+    }
 }
